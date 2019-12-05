ab1de5686a60dca323d850b7ac720604528c71fb
Gene Edwards <eedwards@su.edu>
2019-12-04 19:25:27 -0500

    Implemented day and time with moment.js;

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 $("#intro-header").fadeIn(1500);
 
+var currentDate = document.getElementById("current-date");
+var currentTime = document.getElementById("current-time");
+
+setInterval(() => {
+    var dayNow = moment();
+    var timeNow = moment();
+
+    var day = dayNow.format('dddd - MMMM DD, YYYY ');
+    var time = timeNow.format('h:mma');
+
+    currentDate.textContent = day;
+    currentTime.textContent = time;
+
+}, 1000);
