6bb99dde2b5f8726fbd11a7a57006f2cf71dfff4
Gene Edwards <eedwards@su.edu>
2019-12-04 21:01:41 -0500

    Updated jquery

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $("#intro-header").fadeIn(1500);
 
-var currentDate = document.getElementById("current-date");
-var currentTime = document.getElementById("current-time");
+var currentDate = $("#current-date");
+var currentTime = $("#urrent-time");
 
 setInterval(() => {
     var dayNow = moment();
@@ -10,7 +10,7 @@ setInterval(() => {
     var day = dayNow.format('dddd - MMMM DD, YYYY ');
     var time = timeNow.format('h:mma');
 
-    currentDate.textContent = day;
-    currentTime.textContent = time;
+    currentDate.text(day);
+    currentTime.text(time);
 
 }, 1000);
