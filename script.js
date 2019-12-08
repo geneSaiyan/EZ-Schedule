4088646c0e2b05b1d504d8752fb6d03102ab6e1c
Gene Edwards <eedwards@su.edu>
2019-12-08 00:33:48 -0500

    Updated js file

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ $("#schedule-div").fadeIn(3000);
 //Set date and time variables
 var currentDate = $("#current-date");
 var currentTime = $("#current-time");
+var currentHour = moment();
+var hour = currentHour.format("H");
+var amPm = moment();
+var amOrpm = amPm.format("a");
 
 //Using moment.js to format and display date and time
 setInterval(() => {
@@ -16,6 +20,7 @@ setInterval(() => {
     currentDate.text(day);
     currentTime.text(time);
 
+    updateBckgrnd()
 }, 1000);
 
 //Setting textarea and save button variables
@@ -23,18 +28,24 @@ var txt9am = $("#txt9am");
 var btn9am = $("#btn9am");
 var txt10am = $("#txt10am");
 var btn10am = $("#btn10am");
+var txt11am = $("#txt11am");
+var btn11am = $("#btn11am");
 
 var stickyNote = $("#pinkStickyNote");
 
 //Function to make text display in the textarea elements
 renderTextarea();
 
+//Created function to display textarea values
 function renderTextarea() {
     var txt9amStored = localStorage.getItem("9am");
     txt9am.val(txt9amStored);
 
     var txt10amStored = localStorage.getItem("10am");
     txt10am.val(txt10amStored);
+
+    var txt11amStored = localStorage.getItem("11am");
+    txt11am.val(txt11amStored);
 
     var stickyNoteStored = localStorage.getItem("stickyNote");
     stickyNote.val(stickyNoteStored);
@@ -53,6 +64,11 @@ btn10am.click(function () {
     localStorage.setItem("10am", txt10am.val());
 })
 
+btn11am.click(function () {
+    event.preventDefault();
+
+    localStorage.setItem("11am", txt11am.val());
+})
 
 //When a user clicks out of the sticky note save text
 $("#pinkStickyNote").blur(function () {
@@ -60,3 +76,35 @@ $("#pinkStickyNote").blur(function () {
 
 localStorage.setItem("stickyNote", stickyNote.val());
 });
+
+function updateBckgrnd(){
+    
+    if(hour == 9){
+        
+    }
+    else if (hour == 10){
+
+    }
+    else if (hour == 11){
+
+    }
+    else if (hour == 12){
+        
+    }
+    else if (hour == 13){
+        
+    }
+    else if (hour == 14){
+        
+    }
+    else if (hour == 15){
+        
+    }
+    else if (hour == 16){
+        
+    }
+    else if (hour == 17){
+        
+    }
+}
+
