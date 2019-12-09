2329fef5dce79e14418fd25c1cdc5725fd859b0a
Gene Edwards <eedwards@su.edu>
2019-12-08 20:51:59 -0500

    Updated for loop to dynamially create local storage variables

--- script.js
+++ script.js
@@ -20,32 +20,33 @@ setInterval(() => {
     currentDate.text(day);
     currentTime.text(time);
 
-    updateBckgrnd()
+    updateBckgrndColor()
 }, 1000);
 
 //Putting Schedule times in an array
 var scheduleTimes = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
-//For loop the schedule times and dynamically create variables
+//For loop the schedule times and dynamically create textarea and button variables
 for (var i = 0; i < scheduleTimes.length; ++i) {
-    this["txt"+scheduleTimes[i]] = $("#txt"+scheduleTimes[i]);
-    this["btn"+scheduleTimes[i]] = $("#btn"+scheduleTimes[i]);
+    this["txt" + scheduleTimes[i]] = $("#txt" + scheduleTimes[i]);
+    this["btn" + scheduleTimes[i]] = $("#btn" + scheduleTimes[i]);
 }
 
 //Setting sticky note variable
 var stickyNote = $("#pinkStickyNote");
 
-//Function to make text display in the textarea elements
+//Function to make text display in the textarea elements and sticky note
 renderTextarea();
 
 //Created function to display textarea values
 function renderTextarea() {
 
+    //For loop the schedule times and dynamically create local storage variables
     for (var i = 0; i < scheduleTimes.length; ++i) {
-        this["txt"+scheduleTimes[i]+"stored"] = localStorage.getItem(scheduleTimes[i]);
-        this["txt"+scheduleTimes[i]].val(this["txt"+scheduleTimes[i]+"stored"])
+        this["txt" + scheduleTimes[i] + "stored"] = localStorage.getItem(scheduleTimes[i]);
+        this["txt" + scheduleTimes[i]].val(this["txt" + scheduleTimes[i] + "stored"])
     }
-    
+
     var stickyNoteStored = localStorage.getItem("stickyNote");
     stickyNote.val(stickyNoteStored);
 }
@@ -104,42 +105,43 @@ btn5pm.click(function () {
 
     localStorage.setItem("5pm", txt5pm.val());
 })
+
 //When a user clicks out of the sticky note save text
 $("#pinkStickyNote").blur(function () {
     event.preventDefault();
 
-localStorage.setItem("stickyNote", stickyNote.val());
+    localStorage.setItem("stickyNote", stickyNote.val());
 });
 
 //Update background color function based on hour of the day
-function updateBckgrnd(){
+function updateBckgrndColor() {
 
-    if(hour == 9){
-        
-    }
-    else if (hour == 10){
+    if (hour == 9) {
 
     }
-    else if (hour == 11){
+    else if (hour == 10) {
 
     }
-    else if (hour == 12){
-        
+    else if (hour == 11) {
+
     }
-    else if (hour == 13){
-        
+    else if (hour == 12) {
+
     }
-    else if (hour == 14){
-        
+    else if (hour == 13) {
+
     }
-    else if (hour == 15){
-        
+    else if (hour == 14) {
+
     }
-    else if (hour == 16){
-        
+    else if (hour == 15) {
+
     }
-    else if (hour == 17){
-        
+    else if (hour == 16) {
+
+    }
+    else if (hour == 17) {
+
     }
 }
 
